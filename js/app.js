de61858f3ce90fcf82e24d9ab0a51edbaad6a4e5
Artur Burtsev <artjock@gmail.com>
2012-10-19 23:33:40 +0400

    app view

--- js/app.js
+++ js/app.js
@@ -29,10 +29,25 @@ $(function() {
 
     window.vApp = Backbone.View.extend({
 
+        tagName: 'div',
+        className: 'b-app',
+        template: _.template($("#app-template").html()),
+
         initialize: function() {
+            _.bindAll(this, 'render');
+
+            this.collection.bind('reset', this.render);
         },
 
         render: function() {
+            this.$el.html( this.template({}) );
+            // container for all playlists
+            var $playlists = this.$el.find('.b-app__playlists');
+            // create playlist view for each model in collection
+            this.collection.each(function(playlist) {
+                var view = new vPlaylist({model: playlist});
+                $playlists.append( view.render().$el );
+            });
             return this;
         }
     });
