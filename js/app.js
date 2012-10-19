842a60286f353836dd40044055d5cd3e42f495df
Artur Burtsev <artjock@gmail.com>
2012-10-19 22:41:09 +0400

    playlist view

--- js/app.js
+++ js/app.js
@@ -6,10 +6,17 @@ $(function() {
     // Views
     window.vPlaylist = Backbone.View.extend({
 
+        tagName: 'div',
+        className: 'b-playlist',
+        template: _.template($("#playlist-template").html()),
+
         initialize: function() {
+            _.bindAll(this, 'render');
         },
 
         render: function() {
+            var content = this.template(this.model.toJSON());
+            this.$el.html(content);
             return this;
         }
     });
