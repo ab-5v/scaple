8704bf60d6fef6673c3a72b8eb3fd837e4ceead6
Artur Burtsev <artjock@gmail.com>
2012-10-20 15:21:53 +0400

    check uniq while adding track with bookmarklet

--- js/bookmarklet/server.js
+++ js/bookmarklet/server.js
@@ -1,19 +1,18 @@
 // processing bookmarklet
 (function(d) {
-    var search = d.location.search.slice(1);
-    var pairs = search.split('&');
-    for (var i = 0; i < search.length; i++) {
-        var parts = pairs[0].split('=');
-        if (parts.shift() === 'tr') {
-            if ('localStorage' in window) {
-                var ls = window.localStorage;
-                var tr = JSON.parse(ls.getItem('scaple-tr') || '[]');
-                tr.push( decodeURIComponent(parts.join('=')) );
-                ls.setItem('scaple-tr', JSON.stringify(tr));
-            }
-            // trigger unload to tell iframe "it's time to remove"
-            window.onload();
-            break;
+    var match = document.location.search.match(/tr=([^&]+)/);
+    // TODO: load in frame check, some SOP problems
+    if (match && 'localStorage' in window) {
+        var url = decodeURIComponent(match[1]);
+        // current localstorage state
+        var current = localStorage.getItem('scaple-tr') || '[]';
+        // don't add duplicates
+        if (current.indexOf('"' + url + '"') === -1) {
+            current = JSON.parse(current);
+            current.push(url);
+            localStorage.setItem('scape-tr', JSON.stringify(current));
         }
+        // trigger onload to tell iframe "it's time to remove"
+        window.onload();
     }
 })(document);
