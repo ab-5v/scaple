680d70ac3e9aad6dfd85adf1e96e89c93192c604
Artur Burtsev <artjock@gmail.com>
2012-10-20 16:12:58 +0400

    grab track ids instead of urls

--- js/bookmarklet/client.js
+++ js/bookmarklet/client.js
@@ -1,8 +1,10 @@
 (function(loc) {
-    var str = "!function(d, iframe){\
+    var str = "!function(d, iframe, id){\
+        id = $('.player.large[data-sc-track]').attr('data-sc-track');\
+        if (!id) { return; }\
         iframe = d.createElement('iframe');\
         d.body.appendChild(iframe);\
-        iframe.src = '{URL}?tr=' + encodeURIComponent(d.location.href);\
+        iframe.src = '{URL}?tr=' + encodeURIComponent(id);\
         iframe.style = 'position: absolute; left: -9999px; top: -9999px;';\
         iframe.onload = function() {\
             d.body.removeChild(iframe);\
