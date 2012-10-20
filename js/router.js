bca19749d89b6f484b76818508631e6fcc8d6d58
Artur Burtsev <artjock@gmail.com>
2012-10-20 16:30:51 +0400

    init playlists from bookmarkled localStorage item, fixes #3

--- js/router.js
+++ js/router.js
@@ -10,10 +10,47 @@ Scaple.Router = Backbone.Router.extend({
             collection: this.playlists
         });
         this.playlists.fetch();
+        if (!this.playlists.models.length) {
+            //TODO: add default playlists
+        }
+
+        if ('localStorage' in window) {
+            this.initLocalStorage();
+        }
     },
 
     home: function() {
         this.$root.empty();
         this.$root.append(this.app.render().$el);
+    },
+
+    initLocalStorage: function() {
+        var that = this;
+        $(window).on('storage', function(e){
+            that.loadLocalStorage();
+        });
+
+        this.loadLocalStorage();
+    },
+
+    loadLocalStorage: function() {
+        var that = this;
+        // search for track ids in localStorage
+        var tracks = localStorage.getItem('scaple-tr');
+        if (tracks) {
+            tracks = JSON.parse(tracks);
+            // get tracks by id from SC
+            SC.get('/tracks', {ids: tracks.join(',')}, function(tracks) {
+                // TODO: find active model
+                // add found tracks to model
+                var activeModel = that.playlists.models[0];
+                var current = activeModel.get('tracks');
+                tracks = current.concat(tracks);
+                activeModel.set('tracks', tracks);
+                activeModel.save();
+                // clear found track localStorage
+                localStorage.removeItem('scaple-tr');
+            })
+        }
     }
 });
